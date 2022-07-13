f0100dfc3a2b27f4c550d36152110d56e5a0ca9b
peter41122 <sgreverse0220@gmail.com>
2022-07-13 23:28:39 +0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+import "./App.less";
+import Routes from "./routes";
+
+function App() {
+  return (
+    <>
+      <Routes />
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import { BrowserRouter, Routes as RoutePk, Route } from "react-router-dom";
+
+const Routes = () => {
+  return <></>;
+};
+
+export default Routes;
